59a66f6d1d30fcd4311f79a11fe75a46c7389342
Raphael Britto <raphael.britto@gmail.com>
2017-06-01 01:34:44 -0300

    primeiro commit
    primeiro commit

---
+++ obtem_avaliacao.py
@@ -0,0 +1,11 @@
+
+texto = "Raphael Britto"
+
+for i in texto:
+  print(i)
+
+lista_mercado = ["ovo", "farinha", "leite", "maças"]
+print(lista_mercado)
+print(type(lista_mercado))
+
+print("teste")
---
+++ obtem_avaliacao_csv.py
@@ -0,0 +1,107 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import random
+
+
+# In[70]:
+
+qtd_try = 0
+
+
+# In[72]:
+
+apostas = []
+
+
+# In[73]:
+
+#while (qtd_try < 100):
+for i in range(10):
+    facil = random.sample(range(1, 26), 15)
+    facil.sort()
+    qtd_match = set(a).intersection(set(b))
+    apostas.append(facil)
+
+
+# In[74]:
+
+apostas
+
+
+# In[88]:
+
+apostas[0]
+
+
+# In[80]:
+
+a = list(range(1,6))
+
+
+# In[81]:
+
+a
+
+
+# In[82]:
+
+b = list(range(1,13, 2))
+
+
+# In[83]:
+
+b
+
+
+# In[86]:
+
+c = set(a) & set(b)
+
+
+# In[87]:
+
+c
+
+
+# In[85]:
+
+set(a).intersection(set(b))
+
+
+# In[65]:
+
+facil = random.sample(range(1, 26), 15)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[66]:
+
+type(facil)
+
+
+# In[67]:
+
+facil
+
+
+# In[68]:
+
+facil.sort()
+
+
+# In[69]:
+
+print(facil)
+
