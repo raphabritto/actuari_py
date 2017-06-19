1793892fa53c0def1a9c47a1c3be46a32b5c04cf
Raphael Britto <raphael.britto@gmail.com>
2017-06-18 22:02:02 -0300

    calcula idade
    calcula idade

--- tools.py
+++ tools.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-# import pandas as pd
+import pandas as pd
 from pandas import isnull
 
 def convertToPercent(x):
@@ -48,17 +48,17 @@ def calculateAge(data_nascimento, data_calculo):
     try:
         # if isinstance(data_nascimento, date):
         if not isnull(data_nascimento):
-            data_nascimento = date.strftime(data_nascimento, '%Y-%m-%d')
-            data_calculo = data_calculo.values
+            # data_nascimento = pd.to_datetime(data_nascimento[0])
+            # data_calculo = date.strftime(data_calculo[0], '%Y-%m-%d')
                 # datetime(data_calculo.values, '%Y-%m-%d')
                 # datetime.strftime(parser.parse(data_calculo), '%Y-%m-%d')
-            idade = data_calculo
-            # return int(hoje.year - data_nascimento.year - ((hoje.month, hoje.day) < (data_nascimento.month, data_nascimento.day)))
-
-        return idade
+            # idade = data_nascimento.year
+            idade = int(data_calculo.year - data_nascimento.year - ((data_calculo.month, data_calculo.day) < (data_nascimento.month, data_nascimento.day)))
     except AttributeError as e:
         # print('Erro: ', e)
-        return -1
+        idade = e
+
+    return idade
 
 
 def changeNullToZero(value):
