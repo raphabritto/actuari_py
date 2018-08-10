e33bcc051d4b8ddcb9173256e709ceb6aea2e5be
Raphael Britto <raphael.britto@gmail.com>
2018-08-10 18:29:01 -0300

    iniciando a leitura dos dados participante

--- participante.py
+++ participante.py
@@ -1,7 +1,7 @@
 from datetime import datetime, date
 
 class Participante(object):
-    def __init__(self, nome, data_nascimento):
+    def __init__(self, id, nome, sexo, data_nascimento):
         # print('contruindo a classe participante')
         self.nome = nome
         self.data_nascimento = datetime.strptime(data_nascimento, '%d/%m/%Y').date()
@@ -9,17 +9,3 @@ class Participante(object):
 
     def calcularIdade(self, data_nascimento, data):
         return data.year - data_nascimento.year - ((data.month, data.day) < (data_nascimento.month, data_nascimento.day))
-
-
-
-class Ativo(Participante):
-    def __init__(self, nome, data_nascimento):
-        super().__init__(nome, data_nascimento)
-        # self.nome = nome
-
-
-ativo = Ativo('fulando', '01/01/1990')
-print(ativo.nome)
-print(ativo.data_nascimento)
-print(type(ativo.data_nascimento))
-print(ativo.idade)
